de82c91df31e44e887db48e225b8b7323091c50b
Mukai Wang <wangmk@umich.edu>
2020-06-21 22:40:45 -0400

    Add two scripts that help generate chromosome wise plink files.

---
+++ gene_mapping_chr.R
@@ -0,0 +1,55 @@
+library(biomaRt)
+library(dplyr)
+library(foreach)
+library(intervals)
+
+# setwd("~/UM/Biostatistics/Temporary_RA")
+# i <- Sys.getenv("SLURM_ARRAY_TASK_ID")
+
+mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+
+# allfields = listAttributes(mart)
+
+transcripts = read.table("transcripts.rpkm.tsv", stringsAsFactors = FALSE,
+                         colClasses = c(rep("character", 1), rep("NULL", 86)),
+                         header=TRUE)
+
+extractID = function(fullid){
+  unlist(strsplit(fullid, "[.]"))[1]
+}
+
+transcript_noversion = sapply(transcripts$transcript, extractID) %>% unname()
+
+result = getBM(attributes = c("ensembl_gene_id", "ensembl_transcript_id", 
+                               "chromosome_name", "transcript_start", "transcript_end",
+                               "external_gene_name"),
+                filters = "ensembl_transcript_id",
+                values = transcript_noversion,
+                mart = mart) 
+
+result$transcript_start = ifelse(result$transcript_start>1e5, 
+                                 result$transcript_start-1e5,
+                                 1)
+result$transcript_end = result$transcript_end+1e5
+
+allchrs = c(seq(1,22),c("X","Y"))
+
+foreach(chr=allchrs) %do%{
+  subset = result %>% filter(chromosome_name==chr) %>% 
+    select(chromosome_name, transcript_start, transcript_end)
+  # range set union
+  idf <- Intervals(subset[,2:3])
+  chrunion <- as.data.frame(interval_union(idf))
+  rowsub = nrow(chrunion)
+  chrunion$chrid = rep(chr, rowsub)
+  chrunion$rname = paste0(rep("R", rowsub), 1:rowsub)
+  # move the column position
+  chrunion = chrunion %>% select(chrid, everything())
+  filename = paste0("unionrange/chr", chr, "_range.txt")
+  write.table(chrunion, file=filename, row.names = FALSE, col.names = FALSE, 
+              quote=FALSE)
+  invisible(chr)
+} 
+
+
+
